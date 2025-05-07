5d164120628c7367c89dc8cd9aacf706cd8b76fb
rodri <rherdia609@g.educaand.es>
2025-05-07 20:20:51 +0200

    primer projecto

---
+++ src/calc/Calculadora.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calc;
+
+/**
+ *
+ * @author Alumno Tarde
+ */
+public class Calculadora {
+
+    private int ans;
+
+    public Calculadora() {
+        this.ans = 0;
+    }
+
+    public int suma(int a, int b) {
+        ans = a + b;
+        return ans;
+    }
+
+    
+
+}
